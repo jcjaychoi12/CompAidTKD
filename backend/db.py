a53dba40c30d162d0773fdd4343ea3b060dd44f2
Jungbin Choi <jungbin.l.choi@gmail.com>
2024-05-06 22:11:47 -0400

    Created Matches, Sparrings, Poomsaes

--- backend/db.py
+++ backend/db.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, Float, Boolean, CHAR
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, Float, Boolean, CHAR, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
@@ -27,8 +27,11 @@ class Competitors(Base):
     weight = Column(Float, nullable=False)
     height = Column(Float, nullable=False)
     rank = Column(Integer, nullable=False)
+    tid = Column (Integer, ForeignKey("teams.id")) # One-to-Many --> the Many table will take the foreign key
 
-    team = relationship("Teams", back_populates="competitor")
+    # relatinship("Name of Class", back_populates="name of relationship variable in mentioned class")
+    # Allows automatic changes to be propagated
+    teams = relationship("Teams", back_populates="competitors")
 
     def __repr__(self):
         return f"({self.last_name}, {self.first_name})"
@@ -36,10 +39,43 @@ class Competitors(Base):
 class Teams(Base):
     __tablename__ = "teams"
 
-    cid = Column(Integer, ForeignKey("competitors.id"))
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
-    competitor = relationship("Competitors", back_populates="team")
+    competitors = relationship("Competitors", back_populates="teams")
+
+class Matches(Base):
+    __tablename__ = "matches"
+
+    id = Column(Integer, primary_key=True)
+    match_type = Column("type", CHAR, nullable=False)
+    status = Column(String, nullable=False, default="not started")
+
+    # uselist=False --> returns only one instance and not a list
+    sparrings = relationship("Sparrings", back_populates="matches", uselist=False)
+    poomsaes = relationship("Poomsaes", back_populates="matches")
+
+class Sparrings(Base):
+    __tablename__ = "sparrings"
+    # UniqueConstraint("attribute/column name", name="name of the constraint")
+    # Allows one-to-one relatinship with Matches
+    __table_args__ = (UniqueConstraint("mid", name="sparringPerMatch"),)
+
+    mid = Column(Integer, ForeignKey("matches.id"), primary_key=True)
+    cid = Column(Integer, ForeignKey("competitors.id"))
+    color = Column(String, nullable=False)
+    winner = Column(Integer, nullable=False, default=-1)
+
+    matches = relationship("Matches", back_populates="sparrings")
+
+class Poomsaes(Base):
+    __tablename__ = "poomsaes"
+
+    mid = Column(Integer, ForeignKey("matches.id"))
+    cid = Column(Integer, ForeignKey("competitors.id"))
+    result = Column(Integer, nullable=False, default=-1)
+
+    matches = relationship("Matches", back_populates="poomsaes")
 
 
 # Upload tables into database
