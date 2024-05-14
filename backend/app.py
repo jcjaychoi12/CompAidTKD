8b4df097503c8fbea885066df4cd666970acc185
jcjaychoi12 <jungbin.l.choi@gmail.com>
2024-05-14 15:54:28 -0400

    Initialize routes

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,31 @@
 from flask import Flask
+from db import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>CompAidTKD -- Backend</h1>"
+
+@app.route('/competitors')
+def competitors():
+    return "Competitors"
+
+@app.route('/teams')
+def teams():
+    return "Teams"
+
+@app.route('/matches')
+def matches():
+    return "Matches"
+
+@app.route('/sparrings')
+def sparrings():
+    return "Sparrings"
+
+@app.route('/poomsaes')
+def poomsaes():
+    return "Poomsaes"
 
 
 if __name__ == "__main__":
